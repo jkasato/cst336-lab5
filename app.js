0fbeae51a6236648acfca0098cc21cc8fbd190f4
jkasato <32782614+jkasato@users.noreply.github.com>
2019-07-30 23:09:32 -0700

    Finished video 3 parts: pictures appear in a 3x3 after searching for a keyword

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Installed Nodemon
+// npm i -g nodemon
 //app.js
 const express = require("express");
 const app = express();
@@ -15,27 +17,44 @@ const mysql = require("mysql");
 //root route
 app.get("/", function (req, res) {
     // res.send("it's working!")
-    var requestURL="https://api.unsplash.com/photos/random?client_id=2efa248d55b9d227366470869adfd84bc6cf9800c569e22933499dff6c789a38"
-    request(requestURL, function (error, response, body){//this is Async
+    var requestURL = "https://api.unsplash.com/photos/random?client_id=2efa248d55b9d227366470869adfd84bc6cf9800c569e22933499dff6c789a38"
+    request(requestURL, function (error, response, body) {//this is Async
         // console.log('error:', error); // Print the error if one occurred
         // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // console.log('body:', body); // Print the API data
-        if(!error){
-            var parsedData=JSON.parse(body);
+        if (!error) {
+            var parsedData = JSON.parse(body);
             // console.log("image.url:", parsedData["urls"]["regular"]);
-            var imageURL= parsedData["urls"]["regular"];
-            res.render("index", {"imageURL": imageURL} )
+            var imageURL = parsedData["urls"]["regular"];
+            res.render("index", { "imageURL": imageURL })
         }
-        else{
-            res.render("index", {"error": "Unable to access API"})
+        else {
+            res.render("index", { "error": "Unable to access API" })
         }
 
     });//request
 
-});
+});//root
 
 app.get("/search", function (req, res) {
-    req.send("hi!");
+    // console.dir(req);
+    // console.log(req.query.keyword);
+    var keyword = req.query.keyword;
+    var requestURL = "https://api.unsplash.com/photos/random?query=" + keyword + "&count=9&client_id=2efa248d55b9d227366470869adfd84bc6cf9800c569e22933499dff6c789a38"
+    request(requestURL, function (error, response, body) {//this is Async
+        if (!error) {
+            var parsedData = JSON.parse(body);
+            var imageURLs = [];
+            for (let i = 0; i < 9; i++) {
+                imageURLs.push(parsedData[i].urls.regular);
+            }
+            console.log(imageURLs);
+            res.render("results", { "imageURLs": imageURLs })
+        }
+        else {
+            res.render("results", { "error": "Unable to access API" })
+        }
+    });
 });//search
 
 //server listener
@@ -44,13 +63,11 @@ app.get("/search", function (req, res) {
 // })
 
 //server listener to any request
-// app.listen("8081","0.0.0.0",function(){//port number,ip address
-// app.listen("5500","127.0.0.1",function(){//port number,ip address, callback function to display the message
 app.listen("5500", "127.0.0.1", function () {//port number,ip address
-
     console.log("Express Server is Running...")
 });
 
+//for heroku deployment
 // app.listen(process.env.PORT,process.env.IP,function(){
 //     console.log("Running Express Server...");
 // });
