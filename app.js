8667c6b4d135a87e2c530cd0d62466f36ba299eb
jkasato <32782614+jkasato@users.noreply.github.com>
2019-07-30 22:08:07 -0700

    video 2 material: made random image the background, created header and footer

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 //app.js
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-const request=require("request");
-const mysql=require("mysql");
+const request = require("request");
+
+
+const mysql = require("mysql");
 
 //routes
 
 //root route
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     // res.send("it's working!")
-    res.render("index")
+    var requestURL="https://api.unsplash.com/photos/random?client_id=2efa248d55b9d227366470869adfd84bc6cf9800c569e22933499dff6c789a38"
+    request(requestURL, function (error, response, body){//this is Async
+        // console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the API data
+        if(!error){
+            var parsedData=JSON.parse(body);
+            // console.log("image.url:", parsedData["urls"]["regular"]);
+            var imageURL= parsedData["urls"]["regular"];
+            res.render("index", {"imageURL": imageURL} )
+        }
+        else{
+            res.render("index", {"error": "Unable to access API"})
+        }
+
+    });//request
+
 });
+
+app.get("/search", function (req, res) {
+    req.send("hi!");
+});//search
 
 //server listener
 // app.listen("5500","127.0.0.1",function(){
@@ -24,7 +46,7 @@ app.get("/",function(req,res){
 //server listener to any request
 // app.listen("8081","0.0.0.0",function(){//port number,ip address
 // app.listen("5500","127.0.0.1",function(){//port number,ip address, callback function to display the message
-app.listen("5500","127.0.0.1",function(){//port number,ip address
+app.listen("5500", "127.0.0.1", function () {//port number,ip address
 
     console.log("Express Server is Running...")
 });
