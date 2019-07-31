7018ee0aeda6b1eb39efb01cc00697fa7e6db58e
jkasato <32782614+jkasato@users.noreply.github.com>
2019-07-31 03:17:46 -0700

    another commit after video 5

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,6 +1,7 @@
 //add event listener to an element
 $(document).ready(function(){
     $(".favoriteIcon").on("click",function(){
+        alert($(this).prev().attr("src"));
         if($(this).attr("src")=="img/favorite_off.png"){
             $(this).attr("src","img/favorite_on.png");
         }
